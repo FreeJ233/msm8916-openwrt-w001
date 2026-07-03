e8fdecf1fcbd8b189a68a4d7bb587c1e4e7a1e32
vip9418 <vip9418@outlook.com>
2026-07-03 11:41:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 # echo 'src-git store https://github.com/linkease/istore.git;main' >> feeds.conf.default
 
 # OpenClash代理
-# git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 # turboacc网络加速（需要用户在页面选择 luci-app-turboacc 后，由编译脚本自动执行，此处不再无条件运行）
-# curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # 调试
 # sed -i 's|src-git-full openstick https://github.com/lkiuyu/openstick-feeds.git|src-git-full openstick https://github.com/xuxin1955/openstick-feeds|g' feeds.conf.default
